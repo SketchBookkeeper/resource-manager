bae9723ecc8a004effb1cfcbd939989596713ed6
Paul Allen <p.allen.art@gmail.com>
2018-05-17 23:51:47 -0600

    Add animtion to modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="modal is-active">
-    <div class="modal-background"></div>
-    <div class="modal-card">
-      <header class="modal-card-head">
-        <p class="modal-card-title">{{ title }}</p>
-        <button class="delete" aria-label="close" @click="close"></button>
-      </header>
-      <section class="modal-card-body">
-        <slot></slot>
-      </section>
-      <footer class="modal-card-foot">
-        <slot name="footer"></slot>
-      </footer>
+  <transition name="modal-animation">
+    <div class="modal is-active">
+      <div class="modal-background" @click="close"></div>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">{{ title }}</p>
+            <button class="delete" aria-label="close" @click="close"></button>
+          </header>
+          <section class="modal-card-body">
+            <slot></slot>
+          </section>
+          <footer class="modal-card-foot">
+            <slot name="footer"></slot>
+          </footer>
+        </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -28,3 +30,48 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .modal-animation-enter-active {
+    &,
+    .modal-card {
+      transition: transform 300ms ease-out, opacity 200ms;
+    }
+  }
+
+  .modal-animation-enter {
+    opacity: 0.8;
+    transform: scale(1);
+
+    .modal-card {
+      transform: scale(0.9);
+    }
+  }
+
+  .modal-animation-enter-to {
+    opacity: 1;
+    transform: scale(1);
+
+    .modal-card {
+      transform: scale(1);
+    }
+  }
+
+  .modal-animation-leave-active {
+    &,
+    .modal-card {
+      transition: transform 300ms ease-out, opacity 300ms;
+    }
+  }
+
+  .modal-animation-leave-to {
+    opacity: 0;
+    transform: scale(1);
+
+    .modal-card {
+      transform: scale(0.9);
+      opacity: 0;
+    }
+  }
+
+</style>
