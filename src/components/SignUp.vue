adf90d476a33e0245e6c63020fab4ec0f16af1b6
Paul Allen <p.allen.art@gmail.com>
2018-05-22 23:37:47 -0600

    Create sign up form component.

---
+++ src/components/SignUp.vue
@@ -0,0 +1,50 @@
+<template>
+  <form action="index.html">
+    <div class="field">
+      <div class="control has-icons-left">
+        <input class="input is-medium" type="email" id="email" v-model="email" placeholder="email">
+        <label class="is-invisible" for="email">Email Address</label>
+        <span class="icon is-left">
+          <i class="material-icons">email</i>
+        </span>
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="control has-icons-left">
+        <input class="input is-medium" type="password" id="password" v-model="password" placeholder="password">
+        <label class="is-invisible" for="password">Password</label>
+        <span class="icon is-left">
+          <i class="material-icons">vpn_key</i>
+        </span>
+      </div>
+    </div>
+    <button class="button is-primary is-large" @click="register">Sign Up</button>
+  </form>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+  name: 'SignUp',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register: function (e) {
+      e.preventDefault()
+
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .then(user => {
+          this.$router.push('/')
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
