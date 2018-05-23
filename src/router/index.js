caef975f399ae90d1cc27f57ba36ab8d51f91c93
Paul Allen <p.allen.art@gmail.com>
2018-05-22 23:36:56 -0600

    Add register view.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from '@/router/views/Dashboard'
+import Register from '@/router/views/Register'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Dashboard',
       component: Dashboard
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
---
+++ src/router/views/Register.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hero is-fullheight" >
+    <div class="hero-body">
+      <div class="sign-up box">
+        <h1 class="title is-4">Register For An Account</h1>
+        <SignUp />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SignUp from '@/components/SignUp'
+export default {
+  name: 'Register',
+  components: {
+    SignUp
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .hero {
+    background-image: url(../../assets/register.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+
+    &-body {
+      justify-content: center;
+    }
+  }
+
+  .sign-up {
+    max-width: 600px;
+    width: 100%;
+  }
+</style>
