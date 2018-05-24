f1eddebc2310b99af9cb75f8ccf0c223cea2e95d
Paul Allen <p.allen.art@gmail.com>
2018-05-23 23:15:38 -0600

    Add logout button.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="navbar">
-    <nav class="section level">
+  <div class="section navbar">
+    <nav class="level">
       <div class="level-left">
         <div class="level-item">
           <div class="field has-addons">
@@ -12,18 +12,38 @@
             </p>
           </div>
         </div>
-        <div class="level-right"></div>
+      </div>
+
+      <div class="level-right">
+        <div class="level-item">
+          <button class="button is-light" @click="logout">Logout</button>
+        </div>
       </div>
     </nav>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
-
+  name: 'navbar',
+  methods: {
+    logout: function () {
+      firebase.auth().signOut()
+        .then(() => {
+          this.$router.push('/')
+        })
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  .navbar {
+    width: 100%;
 
+    .level {
+      width: 100%;
+    }
+  }
 </style>
