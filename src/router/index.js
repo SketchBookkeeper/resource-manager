5c94d028688460d9d66eec024e43873789be33e0
Paul Allen <p.allen.art@gmail.com>
2018-05-23 23:15:01 -0600

    Add router guard.

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,69 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase'
+import Home from '@/router/views/Home'
 import Dashboard from '@/router/views/Dashboard'
 import Register from '@/router/views/Register'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
-      name: 'Dashboard',
-      component: Dashboard
+      name: 'home',
+      component: Home,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
       path: '/register',
-      name: 'Register',
-      component: Register
+      name: 'register',
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+// Guard
+// Auth
+router.beforeEach((to, from, next) => {
+  if (
+    to.meta.hasOwnProperty('requiresAuth') &&
+    to.meta.requiresAuth === true
+  ) {
+    if (!firebase.auth().currentUser) {
+      next({
+        path: '/register'
+      })
+    } else {
+      next()
+    }
+  } else if (
+    to.meta.hasOwnProperty('requiresGuest') &&
+    to.meta.requiresGuest === true
+  ) {
+    if (firebase.auth().currentUser) {
+      next({
+        path: '/dashboard'
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
