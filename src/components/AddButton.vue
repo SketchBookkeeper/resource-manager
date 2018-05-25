5010ef5d06860e546807eb412fbee671b456db0d
Paul Allen <p.allen.art@gmail.com>
2018-05-25 08:28:18 -0600

    Combine add to one component.

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -1,23 +1,58 @@
 <template>
   <div class="js-grid-item js-grid-item-stamp">
-    <button @click="showModal" class="add-resource">
+    <button @click="showModal" class="button is-primary add-resource">
       <i class="material-icons large">add_circle_outline</i>
     </button>
 
     <Modal title="Add Resource" v-show="isAddModalVisible" @close="closeModal">
-      <Add />
+      <div>
+        <div class="field">
+          <label class="label">Name</label>
+          <div class="control has-icons-left">
+            <input class="input" type="text" placeholder="How to peel a banana...">
+            <span class="icon is-small is-left">
+              <i class="material-icons">title</i>
+            </span>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Notes</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="Add notes here"></textarea>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">URL</label>
+          <div class="control has-icons-left">
+            <input class="input" type="text" placeholder="https://...">
+            <span class="icon is-small is-left">
+              <i class="material-icons">insert_link</i>
+            </span>
+          </div>
+        </div>
+
+        <div class="control">
+        <button class="button is-primary">
+            <span class="icon is-small">
+            <i class="material-icons large">add_circle_outline</i>
+            </span>
+            <span>Add</span>
+          </button>
+        </div>
+      </div>
     </Modal>
   </div>
 </template>
 
 <script>
-import Add from './Add'
 import Modal from './Modal'
 export default {
   components: {
-    Add,
     Modal
   },
+  props: ['userId'],
   data () {
     return {
       isAddModalVisible: false
@@ -36,12 +71,6 @@ export default {
 
 <style lang="scss" scoped>
   .add-resource {
-    border: 1px dashed #333;
-    border-radius: 5px;
     cursor: pointer;
-    width: 200px;
-    height: 200px;
-    left: 0;
-    top: 0;
   }
 </style>
