78c7937101619a8f56cd80c9a705c0996e9eadd0
Paul Allen <p.allen.art@gmail.com>
2018-05-26 23:17:55 -0600

    Add one option for adding order to new resource.

--- functions/index.js
+++ functions/index.js
@@ -8,35 +8,21 @@ admin.initializeApp(functions.config().firebase);
 
 // Test function
 // orderResources({title: 'test', uid: 'C7fDPD1AkHYZ8dTVKjKX43aiZRe2'}, {params: { resourceId: 'czLMiwiivde2NDWEQECP' }})
-exports.orderResources = functions.firestore
-  .document('resources/{resourceId}')
-  .onCreate((snap, context) => {
-    const data = snap.data();
-    const documentId = context.params.resourceId;
-    const documentRef = admin.firestore().collection('resources').doc(documentId);
+// exports.orderResources = functions.firestore
+//   .document('resources/{resourceId}')
+//   .onCreate((snap, context) => {
+//     const data = snap.data();
+//     const documentId = context.params.resourceId;
+//     const documentRef = admin.firestore().collection('resources').doc(documentId);
 
-    // In preparation for newly added resource
-    // Update other existing resources order by increasing 1
-    const userCollection = admin.firestore()
-      .collection('resources')
-      .where('uid', '==', data.uid)
-      .get()
-      .then(querySnapshot => {
-        return querySnapshot.forEach(doc => {
-          // don't update the newest resource
-          if (data.id === doc.id) return;
-
-          let order = doc.data().order;
-          order++;
-          const documentRef = admin.firestore().collection('resources').doc(doc.id);
-          return documentRef.update({order: order});
-        });
-      })
-      .catch(error => {
-        console.error(error);
-      });
-
-    // Add the order 0 to the newly created resource
-    // Placing it at the beginning of the collection
-    return documentRef.update({order: 0});
-  });
+//     return admin.firestore()
+//       .collection('resources')
+//       .where('uid', '==', data.uid)
+//       .get()
+//       .then(querySnapshot => {
+//         return documentRef.update({order: parseInt(querySnapshot.size - 1)});
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   });
