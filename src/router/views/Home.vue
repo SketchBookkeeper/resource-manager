b7f886e172626ce46fdae7442f2d01de44043245
Paul Allen <p.allen.art@gmail.com>
2018-05-29 21:20:31 -0600

    Add logged out navigation.

--- src/router/views/Home.vue
+++ src/router/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="hero is-fullheight" >
+    <div class="hero-head">
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <div class="navbar-item">
+              <router-link class="button" to="/register">Register</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="hero-body">
       <div class="sign-up box">
         <h1 class="title is-4">Login</h1>
--- src/router/views/Register.vue
+++ src/router/views/Register.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="hero is-fullheight" >
+    <div class="hero-head">
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <div class="navbar-item">
+              <router-link class="button" to="/">Log In</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="hero-body">
       <div class="sign-up box">
         <h1 class="title is-4">Register For An Account</h1>
